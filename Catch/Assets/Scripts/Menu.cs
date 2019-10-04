ecc16d55ac9859195a27e3019fa46b083a26f53f
Ilya873 <36420509+Ilya873@users.noreply.github.com>
2019-10-04 14:48:26 +0300

    сделал меню. теперь надо сделать валюту, чтобы она сохранялась после выхода из игры

---
+++ Catch/Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject Game;
+    public GameObject Skins;
+    public GameObject Graphic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void ClickGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ClickSkins()
+    {
+        Game.active = false;
+        Skins.active = true;
+    }
+
+    public void ClickGraphic()
+    {
+        Game.active = false;
+        Graphic.active = true;
+    }
+
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+
+    public void ClickBack()
+    {
+        Game.active = true;
+        Skins.active = false;
+        Graphic.active = false;
+    }
+}
